757346f2c959d82c574cb984a05fcf758a778c07
Daldanos <adolfogsilveira@gmail.com>
2024-01-08 14:39:01 -0800

    Making changes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,11 +12,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_side;
 
 	if (tree == NULL)
+	{
 		return (0);
+	}
 	else
 	{
-		left_side = binary_tree_height(tree -> left);
-		right_side = binary_tree_height(tree -> right);
+		left_side = binary_tree_height(tree->left);
+		right_side = binary_tree_height(tree->right);
 		if (left_side > right_side)
 		{
 			return (left_side + 1);

ad35d138eb37f1b5f877039ad9435f3977f3c130
Daldanos <adolfogsilveira@gmail.com>
2024-01-08 09:25:29 -0800

    Making changes

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Creates a tree node at right
+ *
+ * @parent: pointer
+ * @value: value
+ * Return: NewNode
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+		return (NULL);
+
+	newNode = binary_tree_node(parent, value);
+
+	if (newNode == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		newNode->right = parent->right;
+		parent->right->parent = newNode;
+	}
+	parent->right = newNode;
+
+	return (newNode);
+}

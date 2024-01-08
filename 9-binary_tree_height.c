a3c66152b9946d901c792def9e0d0f1bb987b294
Daldanos <adolfogsilveira@gmail.com>
2024-01-08 13:36:19 -0800

    Making changes

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Height of the tree
+ *
+ * @tree: tree
+ * Return: Height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftside;
+	size_t rightside;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		leftside = binary_tree_height(tree->left);
+		rightside = binary_tree_height(tree->right);
+		if (leftside > rightside)
+		{
+			return (leftside + 1);
+		}
+		else
+		{
+			return (rightside + 1);
+		}
+	}
+}

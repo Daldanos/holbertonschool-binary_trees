896d9f3a4ae3724ad166976b3eda79c71523ad5c
german <gerchureloco@gmail.com>
2024-01-09 07:49:21 -0800

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_nodes - function name
- * @binary_tree_t: tree
+ * @tree: tree to be count
  *
- * Return counts the nodes with at least 1 child in a binary tree
+ * Return: counts the nodes with at least 1 child in a binary tree
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {

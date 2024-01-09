758db9cf026475c184127a490f39e076e3ef8caa
german <gerchureloco@gmail.com>
2024-01-09 07:47:22 -0800

    13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include"binary_trees.h"
+
+/**
+ * binary_tree_nodes - function name
+ * @binary_tree_t: tree
+ *
+ * Return counts the nodes with at least 1 child in a binary tree
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t left_count, right_count;
+
+	if (tree == NULL)
+		return (0);
+
+	left_count = binary_tree_nodes(tree->left);
+	right_count = binary_tree_nodes(tree->right);
+
+	return (left_count + right_count + (tree->left || tree->right));
+}

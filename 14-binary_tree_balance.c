ee26ef576534ea5a0a56e7bd7fde428750d77297
Daldanos <adolfogsilveira@gmail.com>
2024-01-11 09:20:26 -0800

    Making changes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,8 +10,30 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
 
-	return (left_height - right_height);
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Height of the tree
+ *
+ * @tree: tree
+ * Return: Height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
 }

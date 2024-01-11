e6dc282d5ba46e8c5c693a7d411c2727fb0a29a9
Daldanos <adolfogsilveira@gmail.com>
2024-01-11 09:34:39 -0800

    Making changes

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,19 +9,12 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+	if (node == NULL || node->parent == NULL ||
+		node->parent->parent == NULL ||)
+		return (NULL);
 
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-                return (NULL);
+	if (node->parent-parent-left == node->parent)
+		return (node->parent->parent->right);
 
-	if (grandparent->left && grandparent->right)
-	{
-		if (grandparent->left == parent)
-			return (grandparent->right);
-		else
-			return (grandparent->left);
-	}
-
-	return (NULL);
+	return (node->parent-parent-left);
 }
